48ac496ad95ed07cef90139aaa024e129f2fda71
Peter Berck <pberck@gmail.com>
2021-02-03 13:55:47 +0100

    global.cpp/h files added.

---
+++ src/global.cpp
@@ -0,0 +1,11 @@
+#include "XPLMUtilities.h"
+
+#include "global.h"
+
+namespace XLABEL {
+
+XPLMCommandRef toggle_label_cmd;
+bool show_label = false;
+int  label_kind = 0;
+  
+}
---
+++ src/global.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "XPLMUtilities.h"
+
+namespace XLABEL {
+
+extern XPLMCommandRef toggle_label_cmd;
+extern bool show_label;
+extern int  label_kind;
+  
+}
